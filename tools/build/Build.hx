4e167938f6ddbb5f7584a16431895419c7434aac
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-24 12:27:38 -0300

    Refactor Build class for improved readability

--- tools/build/Build.hx
+++ tools/build/Build.hx
@@ -1,39 +1,36 @@
 class Build extends hxcpp.Builder
 {
-   // Create a build in 'bin' directory, with the "stdlibc++" flags for compatibility
-   //  This flasg should not make a difference because hxcpp does not use stdlibc++
-   override public function wantLegacyIosBuild() { return true; }
+    override public function wantLegacyIosBuild():Bool { return true; }
+    override public function wantWindows64():Bool { return true; }
 
-   override public function wantWindows64() { return true; }
+    override public function runBuild(target:String, isStatic:Bool, arch:String, inFlags:Array<String>):Void
+    {
+        var here:String = Sys.getCwd().split("\\").join("/");
 
+        var parts:Array<String> = here.split("/");
+        while (parts.length > 0 && parts[parts.length - 1] == "")
+            parts.pop();
+        if (parts.length > 0)
+            parts.pop();
 
-   // Override to ensure this version if hxcpp is used, even if haxelib says otherwise
-   override public function runBuild(target:String, isStatic:Bool, arch:String, inFlags:Array<String>)
-   {
-       var args = ["run.n", "Build.xml"].concat(inFlags);
-       var here = Sys.getCwd().split("\\").join("/");
+        var hxcppDir:String = parts.join("/");
+        var args:Array<String> = ["run.n", "Build.xml"].concat(inFlags).concat([here]);
 
-       var parts = here.split("/");
-       if (parts.length>0 && parts[parts.length-1]=="")
-          parts.pop();
-       if (parts.length>0)
-          parts.pop();
-       var hxcppDir = parts.join("/");
+        Sys.setCwd(hxcppDir);
+        Sys.println("neko " + args.join(" "));
 
-       // This is how haxelib calls a 'run.n' script...
-       Sys.setCwd(hxcppDir);
-       args.push(here);
-       Sys.println("neko " + args.join(" ")); 
-       if (Sys.command("neko",args)!=0)
-       {
-          Sys.println("#### Error building neko " + inFlags.join(" "));
-          Sys.exit(-1);
-       }
-       Sys.setCwd(here);
-   }
+        var exitCode:Int = Sys.command("neko", args);
+        if (exitCode != 0)
+        {
+            Sys.println('#### Build failed (exit $exitCode): neko ${inFlags.join(" ")}');
+            Sys.exit(exitCode);
+        }
 
-   public static function main()
-   {
-      new Build( Sys.args() );
-   }
+        Sys.setCwd(here);
+    }
+
+    public static function main():Void
+    {
+        new Build(Sys.args());
+    }
 }
